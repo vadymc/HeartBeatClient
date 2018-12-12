c7a027cc8aba8b5c70647b68a8e6b8458918087e
vadymc <vadym.chekrii@transferwise.com>
2018-12-12 13:29:04 +0200

    firebase cloud messaging integration

--- app/src/main/java/vadc/heartbeatclient/MainActivity.kt
+++ app/src/main/java/vadc/heartbeatclient/MainActivity.kt
@@ -1,12 +1,16 @@
 package vadc.heartbeatclient
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+
+    private var notificationHelper: NotificationHelper? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        notificationHelper = NotificationHelper(this)
     }
 }
---
+++ app/src/main/java/vadc/heartbeatclient/NotificationHelper.kt
@@ -0,0 +1,38 @@
+package vadc.heartbeatclient
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.ContextWrapper
+import android.graphics.Color
+
+class NotificationHelper(ctx: Context) : ContextWrapper(ctx) {
+
+    private val channel = "default"
+
+    private val manager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+    init {
+        val channel = NotificationChannel(
+            channel,
+            getString(R.string.default_notification_channel_name), NotificationManager.IMPORTANCE_HIGH
+        )
+        channel.lightColor = Color.GREEN
+        channel.lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+        manager.createNotificationChannel(channel)
+    }
+
+    fun notify(title: String, body: String) {
+        manager.notify(1001, buildNotification(title, body))
+    }
+
+    private fun buildNotification(title: String, body: String): Notification {
+        return Notification.Builder(applicationContext, channel)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setSmallIcon(android.R.drawable.stat_notify_chat)
+            .setAutoCancel(true)
+            .build()
+    }
+}
